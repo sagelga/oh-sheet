84ab2ce2d365723260fa68fbdb359d39a75c715f
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2018-09-03 19:54:18 +0700

    Add Vuesax

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
+import Vuesax from 'vuesax';
+import 'vuesax/dist/vuesax.css';
 import App from './App.vue';
 import router from './router';
 import './registerServiceWorker';
 
+Vue.use(Vuesax);
 Vue.config.productionTip = false;
 
 new Vue({
