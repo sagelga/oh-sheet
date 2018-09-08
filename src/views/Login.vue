1be3d0ed4a73bf12ac081618fa9cc3194819d819
son9912 <son9912@gmail.com>
2018-09-08 12:57:25 +0700

    Land a new Login page

---
+++ src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="login">
+    <h1>Sign in. For all Lecture.</h1>
+    <h3>Please try not to hack us. lel.</h3>
+
+    <el-input placeholder="Please input password" v-model="username_input"></el-input>
+    <el-input placeholder="Please input password" v-model="password_input"></el-input>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      input: 'Username',
+    };
+  },
+};
+</script>
+
+<!--<style scoped>-->
+
+<!--</style>-->
