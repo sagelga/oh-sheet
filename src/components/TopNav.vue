8da308618cea311de01d72273eb5f9d6b5c68bf2
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2018-09-08 15:32:26 +0700

    Use icons on TopNav

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -2,11 +2,17 @@
   <BoxedContainer>
     <el-menu :default-active="defaultActive" mode="horizontal"
       router="true" style="text-align: right;">
-      <el-menu-item index="/">ค้นหา</el-menu-item>
-      <el-menu-item index="/saved">คอลเลคชัน</el-menu-item>
-      <el-menu-item index="/upload">อัปโหลด</el-menu-item>
-      <el-submenu>
-        <template slot="title">โปรไฟล์</template>
+      <el-menu-item index="/">
+        <i class="el-icon-search"></i>
+      </el-menu-item>
+      <el-menu-item index="/saved">
+        <i class="el-icon-star-on"></i>
+      </el-menu-item>
+      <el-menu-item index="/upload">
+        <i class="el-icon-upload"></i>
+      </el-menu-item>
+      <el-submenu id="topnav-more">
+        <template slot="title"><i class="el-icon-setting"></i></template>
         <el-menu-item index="/profile/zartre">โปรไฟล์ของฉัน</el-menu-item>
         <el-menu-item index="/logout">ออกจากระบบ</el-menu-item>
       </el-submenu>
@@ -19,6 +25,9 @@
   .el-menu--horizontal > .el-submenu {
     float: none;
     display: inline-block;
+  }
+  .el-menu--horizontal > .el-submenu#topnav-more i.el-submenu__icon-arrow {
+    display: none;
   }
 </style>
 
