b0d1fec981ff462a8547827267368d6269954433
son9912 <son9912@gmail.com>
2018-09-08 21:09:42 +0700

    Finished up with login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,26 +1,56 @@
 <template>
-<div class="login">
-    <h1>Sign in. For all Lecture.</h1>
-    <h3>Please try not to hack us. lel.</h3>
+    <div class="login">
+        <div class="grid-content">
 
-    <el-input placeholder="Please input password" v-model="username_input"></el-input>
-    <el-input placeholder="Please input password" v-model="password_input"></el-input>
-    <vs-button v-on:click="type = disabled">Sign In</vs-button>
-</div>
+            <h1>Sign in.</h1>
+            <h3>For all Lecture.</h3>
+
+            <div>
+
+                <div><!--Username input-->
+                    <el-input v-model="username" clearable="" size="large">
+                        <template slot="prepend">Username</template>
+                    </el-input>
+                </div>
+
+                <div><!--Password input-->
+                    <el-input v-model="password" clearable="" size="large">
+                        <template slot="prepend">Password</template>
+                    </el-input>
+                </div>
+            </div>
+
+            <br/>
+
+            <!--Remember Password-->
+            <el-switch
+                    v-model="rememberPassword"
+                    active-color="#13ce66"
+                    inactive-color="#909399"
+                    active-value="True"
+                    inactive-value="False"
+                    active-text="Remember password">
+            </el-switch>
+            <br><br>
+
+            <!--Sign In / Sign Up Button-->
+            <el-button type="primary" round>Login</el-button>
+            <el-button type="primary" round>Sign Up</el-button>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'Login',
-  data() {
-
-    return {
-        sign_in_click: false,
-      username_input: 'Username',
-        password_input: 'Password',
+    export default {
+        name: 'Login',
+        data() {
+            return {
+                username: '',
+                password: '',
+                rememberPassword: 'False',
+            };
+        },
     };
-  },
-};
 </script>
 
 <!--<style scoped>-->
