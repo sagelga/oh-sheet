08e9c64c080c94a4ab95f141ed4d3402cb08deaa
son9912 <son9912@gmail.com>
2018-09-09 12:35:43 +0700

    Covered login page with form and layout container

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,58 +1,103 @@
 <template>
     <div class="login">
-        <div class="grid-content">
+        <el-row :gutter="10">
 
-            <h1>Sign in.</h1>
-            <h3>For all Lecture.</h3>
+            <el-col :xs="1" :sm="1" :md="4" :lg="6" :xl="1">
+                <div class="grid-content"></div>
+            </el-col>
 
-            <div>
+            <el-col :xs="22" :sm="22" :md="16" :lg="12" :xl="11">
+                <div class="bg-purple-light">
+                    <h1>Sign in. For all Lecture.</h1>
 
-                <div><!--Username input-->
-                    <el-input v-model="username" clearable="" size="large">
-                        <template slot="prepend">Username</template>
-                    </el-input>
+                    <form>
+                        <div><!--Username input-->
+                            <p align="left">Username</p>
+                            <el-input v-model="username"
+                                      clearable=""
+                                      size="large"
+                                      class="inputTextLogin"
+                                      auto-complete="on"
+                                      type="text">
+                            </el-input>
+                        </div>
+
+                        <div><!--Password input-->
+                            <p align="left">Password</p>
+                            <el-input v-model="password"
+                                      clearable=""
+                                      size="large"
+                                      class="inputTextLogin"
+                                      auto-complete="on"
+                                      type="password">
+                            </el-input>
+                        </div>
+
+                        <br/>
+
+                        <!--Remember Password-->
+                        <el-switch
+                                v-model="rememberPassword"
+                                active-color="#13ce66"
+                                inactive-color="#909399"
+                                active-value="True"
+                                inactive-value="False"
+                                active-text="Remember password">
+                        </el-switch>
+                        <br/><br/>
+
+                        <!--Sign In / Sign Up Button-->
+                        <el-button type="primary" round>Login</el-button>
+                        <el-button type="primary" round>Sign Up</el-button>
+
+                    </form>
                 </div>
+            </el-col>
 
-                <div><!--Password input-->
-                    <el-input v-model="password" clearable="" size="large">
-                        <template slot="prepend">Password</template>
-                    </el-input>
-                </div>
-            </div>
+            <el-col :xs="1" :sm="1" :md="4" :lg="6" :xl="1">
+                <div class="grid-content"></div>
+            </el-col>
 
-            <br/>
-
-            <!--Remember Password-->
-            <el-switch
-                    v-model="rememberPassword"
-                    active-color="#13ce66"
-                    inactive-color="#909399"
-                    active-value="True"
-                    inactive-value="False"
-                    active-text="Remember password">
-            </el-switch>
-            <br><br>
-
-            <!--Sign In / Sign Up Button-->
-            <el-button type="primary" round>Login</el-button>
-            <el-button type="primary" round>Sign Up</el-button>
-        </div>
+        </el-row>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'Login',
-        data() {
-            return {
-                username: '',
-                password: '',
-                rememberPassword: 'False',
-            };
-        },
+export default {
+  name: 'Login',
+  data() {
+    return {
+      username: '',
+      password: '',
+      rememberPassword: 'False',
     };
+  },
+
+  // methods: {
+  //   onSubmit() {
+  //     console.log('Submit the login form !!');
+  //   },
+  // },
+
+};
 </script>
 
-<!--<style scoped>-->
+<style scoped>
+    .el-main {
+        background-color: #E9EEF3;
+        color: #333;
+        width: 50%;
+        alignment: center;
+    }
 
-<!--</style>-->
+    .bg-purple-light {
+        background: #e5e9f2;
+    }
+
+    .grid-content {
+        border-radius: 4px;
+        min-height: 36px;
+    }
+
+
+</style>
