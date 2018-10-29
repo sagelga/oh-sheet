a578819064d320bb3049d11ab7a51e10597c1d5e
tiltgod <59070084@it.kmitl.ac.th>
2018-10-29 14:02:10 +0700

    footer
    not done yet

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+	<Boxedcontainer>
+		<h1>Allahu akbar</h1>
+	</Boxedcontainer>
+</template>
+<script>
+	import BoxedContainer from '@/components/BoxedContainer.vue';
+	import store from '@/store';
+	export default {
+		name: 'Footer',
+		components: {
+			Boxedcontainer,
+		}
+	},
+</script>
