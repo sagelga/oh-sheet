30bd4af0a3216b4e6bf13a2bbda5b6a8a0e3125c
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2018-12-14 21:05:58 +0700

    Limit accepted file type to PDF

--- src/views/UploadLecture.vue
+++ src/views/UploadLecture.vue
@@ -35,6 +35,7 @@
             </el-form-item>
             <div>
               <label class="el-form-item__label" style="float: none;">อัปโหลดไฟล์ PDF</label>
+              <div style="font-size:0.85em; letter-spacing: 0.03em">ขนาดไฟล์ไม่เกิน 5 MB</div>
               <el-alert title="ตอนนี้มีไฟล์อยู่แล้ว แต่หากต้องการเปลี่ยนให้ลากไฟล์มาใหม่ได้เลย"
                         type="warning" :closable="false" show-icon
                         v-show="formData.objectId"></el-alert>
@@ -234,6 +235,7 @@ export default {
       paramName: 'upload',
       maxFiles: 1,
       maxFilesize: 5, // MB
+      acceptedFiles: 'application/pdf',
       headers: {
         'Cache-Control': '',
         'X-Requested-With': '',
